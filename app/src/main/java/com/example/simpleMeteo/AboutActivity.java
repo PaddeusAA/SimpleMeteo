6b43a0bacfc40e11e42479cfc4f127e9d5e7c3cc
Vladislav Desktop <PaddeusVladislav@gmail.com>
2023-03-30 17:39:38 +0400

    восстановленный проект. Дипломная версия 2.0

---
+++ app/src/main/java/com/example/simpleMeteo/AboutActivity.java
@@ -0,0 +1,40 @@
+package com.example.simpleMeteo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.simpleMeteo.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    // ссылка для GitHub
+    public void gitLink (View v){
+        Uri uri = Uri.parse("https://github.com/PaddeusAA/SimpleMetio");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    //ссылка на tg
+    public void tgLink (View v){
+        Uri uri = Uri.parse("https://t.me/LinusTorvalds_337");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    //кнопка перехода на основной слой
+    public void buttSwitchToDesiredA(View v) {
+        Intent intent = new Intent(this, DesiredCityActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+    }
+}
